918cea0ef3c3b75e731ef24ef8c3343ecc24c857
ysh2987 <ysh2987@naver.com>
2022-04-01 23:08:25 +0900

    Feat: banner

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <StyledHome>
+      <section className="banner">
+        <p className="title">
+          원하는 지역과 스포츠를
+          <br />
+          함께할 팀원을 찾는 방법
+        </p>
+        <p>
+          <span>Team Finder</span>에서 함께 할 팀원을 찾아보세요!
+        </p>
+        <img src="/images/team.png" alt="we are the team" />
+      </section>
+      <section>카드리스트</section>
+    </StyledHome>
+  );
 };
 
 export default Home;
+
+const StyledHome = styled.main`
+  .banner {
+    text-align: center;
+    p {
+      font-size: 1.5rem;
+    }
+    .title {
+      font-size: 2rem;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    span {
+      font-family: 'Anton', sans-serif;
+    }
+    img {
+      width: 100%;
+      max-width: 800px;
+    }
+  }
+`;
